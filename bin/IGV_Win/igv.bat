7176c5e5df9e0d0a0dab7c21c3fffd42ea09767a
nak <wfnakamura@gmail.com>
2019-09-02 15:02:02 +0900

    edit igv start batch

--- bin/IGV_Win/igv.bat
+++ bin/IGV_Win/igv.bat
@@ -1,4 +1,11 @@
 ::Get the current batch file's short path
+
+set JAVA_HOME=%~dp0..\jre
+set PATH=%JAVA_HOME%\bin;%PATH%
+
+echo %JAVA_HOME%
+echo %PATH%
+
 for %%x in (%0) do set BatchPath=%%~dpsx
 for %%x in (%BatchPath%) do set BatchPath=%%~dpsx
 java -Xmx4g -Dproduction=true -Djava.net.preferIPv4Stack=true -Dsun.java2d.noddraw=true -jar %BatchPath%\lib\igv.jar  %*
